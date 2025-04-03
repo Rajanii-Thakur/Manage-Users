626ce9adbfb943396f848e5eff65d8ec8061934b
Rajanii-Thakur <rajanithakur416@gmail.com>
2025-04-03 15:18:19 +0530

    Update deleteuser.php

--- Project2/deleteuser.php
+++ Project2/deleteuser.php
@@ -5,7 +5,8 @@ $id = $_GET['id'];
 $sql = "DELETE From myuser WHERE id = '$id'";
 
 if ($connection->query($sql) === TRUE) {
-    echo 'Deleted Successfully';
+    echo 'Deleted Successfully.<br>
+                <a href="userlist.php" class="btn btn-info mt-3">View Records</a>';
 } else {
     echo "Error: " . $connection->error;
 }
